16202e75be8edee8ab71e9e3bacbd09389ebd6f7
007steve <stephen.plummer1.sp@gmail.com>
2020-08-02 22:44:25 -0400

    added nav bar

---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './nav.css';
+const Nav = () => {
+    return (
+        <nav>
+        <ul>
+            <img className="nav--logo" src="https://cdn.vox-cdn.com/thumbor/GS5aVofpjj3xAZmMMp2hBuYGmpE=/0x133:3151x1905/1600x900/cdn.vox-cdn.com/uploads/chorus_image/image/49901753/netflixlogo.0.0.png" alt="logo"/>
+          <li>Home</li>
+          <li>Show Page</li>
+        </ul>
+      </nav>
+      );
+}
+ 
+export default Nav;
