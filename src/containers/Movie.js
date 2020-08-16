03971d3aef29dec54850c627de5ca9a664df9314
007steve <stephen.plummer1.sp@gmail.com>
2020-08-16 15:29:46 -0400

    added redux to get movies

--- src/containers/Movie.js
+++ src/containers/Movie.js
@@ -1,21 +1,30 @@
 import React, { Component } from 'react'
 import MovieCard from '../components/MovieCard'
 import MovieForm from './MovieForm'
-
-
+import { connect } from 'react-redux'
+import { getMovies } from '../actions/movies'
 class Movie extends Component {
     
+    componentDidMount(){
+        this.props.getMovies()
+    }
     
     render() { 
         return (   
-         <div className="Movie-Container">
-            <h1>Movies component</h1>
-            {this.props.movies.map(movie => <MovieCard key={movie.id}movie={movie} />)}
-            <MovieForm />
-         </div> 
+             <div className="Movie-Container">
+                     {this.props.movies.map(movie => 
+                          <div key={movie.id} className="Movie-Card">
+                              <img className="Movie-Image" src={movie.image} alt={movie.title}/>
+                           </div> )}
+             </div> 
         
          );
     }
-}
+ }
  
-export default Movie;
+ const mapStateToProps = (state) => {
+    return ({
+        movies: state.movies
+    })
+}
+export default connect(mapStateToProps,{ getMovies })(Movie);
