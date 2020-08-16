89bd6f6060d068ef457e24a05ab403a552d098a8
007steve <stephen.plummer1.sp@gmail.com>
2020-08-16 01:47:25 -0400

    add comments to update nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './nav.css';
+//import { Link,NavLink, Router } from 'react-router-dom';
 const Nav = () => {
     return (
-        <nav>
-        <ul>
+      // need to add links FIX 
+    
+       <nav>
+          <ul>
             <img className="nav--logo" src="https://cdn.vox-cdn.com/thumbor/GS5aVofpjj3xAZmMMp2hBuYGmpE=/0x133:3151x1905/1600x900/cdn.vox-cdn.com/uploads/chorus_image/image/49901753/netflixlogo.0.0.png" alt="logo"/>
-          <li>Home</li>
-          <li>Show Page</li>
-        </ul>
+             <li>Home</li>
+             <li>Show Page</li>
+          </ul>
       </nav>
+      
       );
 }
  
