5dc20dabb6d486c5bbdb11d9430134dd7358cbbe
007steve <stephen.plummer1.sp@gmail.com>
2020-08-16 15:34:24 -0400

    added reducers

---
+++ src/reducers/movies.js
@@ -0,0 +1,10 @@
+
+export default (state = [], action) => {
+    switch(action.type) {
+        case 'GET_MOVIES': 
+        return action.movies;
+
+        default:
+            return state;
+    }
+ }
