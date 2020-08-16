34d8f014fa7833158cba60ab73ecb526cec46ba0
007steve <stephen.plummer1.sp@gmail.com>
2020-08-16 15:31:33 -0400

    clean up app.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,53 +1,32 @@
 import './App.css';
 import Header from '../components/Header';
 import React, { Component } from 'react';
-import MovieCard from '../components/MovieCard'
 import Row from './row';
 import Show from './Show'
-import store from '../store.js'
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   //Link
 } from "react-router-dom";
-import Movie from './Movie'
-//import Home from './Home';
+
+
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      movies: []
-    }
-  }
-  componentDidMount(){
-    fetch('http://localhost:3000/movies')
-     .then(respone => respone.json())
-     .then(movies => this.setState({ movies }))
-  }
-  
   
   render(){
-    debugger
-    console.log(store)
+    
   return (
     <Router>
       <Switch>
-        
-      <Route exact  path="/movies:id"  component={Show}/> 
-    <div className="App">
-      <Header/>
-      <Row title={"Recently Added"} movies={this.state.movies}/>
-      <Row title={"Trending Now"} movies={this.state.movies}/>
-      <Row title={"Popular on Netflix"} movies={this.state.movies}/>
-      <MovieCard  movie={this.state.movies} />
-      <Movie movies={this.state.movies} />
-      <Movie movies={this.state.movies} />
-  
-    </div>
-    </Switch>
+       <Route exact  path="/movies:id"  component={Show}/> 
+        <div className="App">
+         <Header/>
+         <Row title="Recently Added" />
+         <Row title="Trending Now" />
+         <Row title="Popular on Netflix" />
+       </div>
+     </Switch>
     </Router>
   );
   }
