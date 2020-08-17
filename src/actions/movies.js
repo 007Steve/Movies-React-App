49b67e151ce64b03fcfe78156643f68373b42e70
007steve <stephen.plummer1.sp@gmail.com>
2020-08-16 20:28:20 -0400

    added addMovie to actions

--- src/actions/movies.js
+++ src/actions/movies.js
@@ -1,8 +1,16 @@
+import { resetMovieForm } from './movieForm'
+
 //Action creator 
 const setMovies = movies => {
     return {
          type: 'GET_MOVIES',
          movies 
+    }
+}
+const addMovie = movie => {
+    return {
+        type:'CREATE_MOVIE',
+        movie 
     }
 }
 
@@ -13,5 +21,24 @@ const setMovies = movies => {
          .then(respone => respone.json())
          .then(movies => dispatch(setMovies(movies)))
          .catch(error => console.log(error))
+    }
+}
+
+export const createMovie = movie => {
+    return dispatch => {
+        return fetch("http://localhost:3000/movies", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({movie: movie})
+        })
+        .then(response => response.json())
+        .then(movie => 
+            dispatch(addMovie(movie)),
+            dispatch(resetMovieForm())
+        )
+        .catch(error => console.log(error));
+
     }
 }
