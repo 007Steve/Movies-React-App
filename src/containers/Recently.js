6a128a18fb373add9205288d5f596a3fc6fa37fd
007steve <stephen.plummer1.sp@gmail.com>
2020-08-17 03:09:10 -0400

    added links

--- src/containers/Recently.js
+++ src/containers/Recently.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { getMovies } from '../actions/movies'
+import { Link } from 'react-router-dom'
 class Movie extends Component {
     
     componentDidMount(){
@@ -10,14 +11,15 @@ class Movie extends Component {
     render() { 
         return (   
             <div>
-            <h1 className="title-text">Recently Added</h1>
-             <div className="Movie-Container">
-                 
+                 <h1 className="title-text">Recently Added</h1>
+                <div className="Movie-Container">
                      {this.props.movies.reverse().map(movie => 
                           <div key={movie.id} className="Movie-Card">
+                              <Link to={'/movies/' + movie.id} >
                               <img className="Movie-Image" src={movie.image} alt={movie.title}/>
+                              </Link>
                            </div> )}
-             </div> 
+                 </div> 
              </div>
          );
     }
