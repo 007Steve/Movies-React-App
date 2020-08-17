264e8301fae3d4fc3fb8083fb9303b6394a20e22
007steve <stephen.plummer1.sp@gmail.com>
2020-08-17 03:16:08 -0400

    added links to li and img

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import './nav.css';
-//import { Link,NavLink, Router } from 'react-router-dom';
+import { Link} from 'react-router-dom';
+
 const Nav = () => {
     return (
       // need to add links FIX 
     
        <nav>
           <ul>
-            <img className="nav--logo" src="https://cdn.vox-cdn.com/thumbor/GS5aVofpjj3xAZmMMp2hBuYGmpE=/0x133:3151x1905/1600x900/cdn.vox-cdn.com/uploads/chorus_image/image/49901753/netflixlogo.0.0.png" alt="logo"/>
-             <li>Home</li>
-             <li>Show Page</li>
+            <Link to="/" ><img className="nav--logo" src="https://cdn.vox-cdn.com/thumbor/GS5aVofpjj3xAZmMMp2hBuYGmpE=/0x133:3151x1905/1600x900/cdn.vox-cdn.com/uploads/chorus_image/image/49901753/netflixlogo.0.0.png" alt="logo"/></Link>
+             <li><Link to="/" >Home</Link></li>
+             <li><Link to="/movies/new" >Add MOVIE</Link></li>
           </ul>
       </nav>
       
