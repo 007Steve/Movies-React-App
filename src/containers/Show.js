faf203a071aeb0d88c8366f8644095d543ef2f04
007steve <stephen.plummer1.sp@gmail.com>
2020-08-16 23:23:44 -0400

    added show page

--- src/containers/Show.js
+++ src/containers/Show.js
@@ -1,22 +1,38 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+import movies from '../reducers/movies';
+
+
 class Show extends Component {
   
-  state = { 
+    state = { movies:[] }
   
-   }
 
-   
-  
-;
+  componentDidMount(){
+    let id = this.props.match.params.id
+   fetch(`http://localhost:3000/movies/${id}`)
+        .then(respone => respone.json())
+        .then(movies => { 
+          this.setState({ movies: movies})
+           console.log(movies)
+         })
+  }
   render() { 
-   
+debugger
     return ( 
-      <div>
-     <h1>SHOW Page for </h1>
-    
+      <div className="show-container">
+        <img className="show-image" src={this.state.movies.image} />
+        <h1 className="show-title" >{this.state.movies.title}</h1>
+        <p className="show-description">{this.state.movies.description}</p>
+         <h3 className="show-category">{this.state.movies.category}</h3>
+        <p className="show-year">{this.state.movies.year}</p>
       </div>
-      );
+    
+     );
   }
 }
  
 export default Show;
+
+
+
+ 
